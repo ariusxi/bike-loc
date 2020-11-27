35a900f41e4c00cac5ddd8eef28403969e1802bc
Camilla Freitas <camirsfreitas@gmail.com>
2020-11-27 16:24:41 -0300

    Sign in and sign up buttons created

--- app/src/main/java/br/com/felix/bikeloc/ui/LoginFormActivity.java
+++ app/src/main/java/br/com/felix/bikeloc/ui/LoginFormActivity.java
@@ -3,17 +3,27 @@ package br.com.felix.bikeloc.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.felix.bikeloc.R;
 
 public class LoginFormActivity extends AppCompatActivity {
 
+    private EditText editTextLoginEmail;
+    private EditText editTextLoginSenha;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+        editTextLoginEmail = findViewById(R.id.editTextLoginEmail);
+        editTextLoginSenha = findViewById(R.id.editTextLoginSenha);
+        auth = FirebaseAuth.getInstance();
 
         findViewById(R.id.textCreateAccount).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +31,17 @@ public class LoginFormActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
             }
         });
+    }
+
+    public void login(View view) {
+        String email = editTextLoginEmail.getEditableText().toString();
+        String senha = editTextLoginSenha.getEditableText().toString();
+        auth.signInWithEmailAndPassword(email, senha).
+                addOnSuccessListener((result) -> {
+                    startActivity(new Intent (this, MainActivity.class));
+                }).
+                addOnFailureListener((error) -> {
+                    error.printStackTrace();
+                });
     }
 }
